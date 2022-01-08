cb8c6b9f215e315a62e846bfaef800326479b7bf
pandanmochi <to.un.thi@gmail.com>
2022-01-08 21:18:46 +0100

    change name style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ export default function Navbar() {
   return (
     <header className="bg-dark-blue md:sticky top-0 z-50">
       <div className="container mx-auto flex flex-wrap p-5 flex-col lg:flex-row">
-        <div className="text-center text-4xl lg:text-5xl font-gravitas text-golden-yellow mb-4 lg:mb-0">
-          TO UYEN NG.
+        <div className="text-center text-3xl lg:text-3xl text-golden-yellow font-bold mb-4 lg:mb-0">
+          Tố Uyên Nguyễn
         </div>
-        <nav className="font-raleway text-xl lg:text-2xl text-white font-bold lg:mr-auto lg:ml-6 lg:py-1 lg:pl-20 flex flex-wrap justify-around">
+        <nav className=" text-xl lg:text-2xl text-white font-bold lg:mr-auto lg:ml-6 lg:py-1 lg:pl-20 flex flex-wrap justify-around">
           <a
             href="#about"
             className="lg:mr-8 hover:text-golden-yellow transition duration-700"
